e315db3bd31973c14cffcc7c21ce3bad6bb8db23
akhandpratap.singh <akhandpratap.singh@servicenow.com>
2023-08-06 20:06:46 +0530

    adding fs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const axios = require('axios');
-const fs = require('fs-extra')
+const fs = require('fs');
 //const request = require('request');
 
 (async function main() {
